cb609a0191eb75fdfdf882280490498be14a8aba
Parasite-231 <muktadir@iut-dhaka.edu>
2024-08-18 21:27:44 +0600

    done

--- client/src/app/Routes/client.routes.ts
+++ client/src/app/Routes/client.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from '../client/component/home/home.component';
 import { NotFoundComponent } from '../error/not-found/not-found.component';
 import { WelcomeComponent } from '../welcome.component';
 import { EventsComponent } from '../client/component/events/events.component';
+import { EventDetailsComponent } from '../client/component/events/event-details.component';
 
 export const clientRoutes: Routes = [
   {
@@ -17,6 +18,10 @@ export const clientRoutes: Routes = [
   {
     path: 'events',
     component: EventsComponent,
+  },
+  {
+    path: 'events/:id',
+    component: EventDetailsComponent,
   },
   {
     path: '**',
